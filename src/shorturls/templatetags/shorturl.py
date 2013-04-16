002869e31d4488a7d4b5d8349d69d1c566a1cdae
Flavio Curella <flavio.curella@gmail.com>
2013-04-16 12:07:04 -0500

    refactored template tags in order to add a ``short url`` filter

--- src/shorturls/templatetags/shorturl.py
+++ src/shorturls/templatetags/shorturl.py
@@ -5,6 +5,34 @@ from django.core import urlresolvers
 from django.utils.safestring import mark_safe
 from shorturls import default_converter as converter
 
+
+PREFIXMAP = {m: p for p, m in settings.SHORTEN_MODELS.items()}
+
+
+def _get_prefix(model):
+    key = '%s.%s' % (model._meta.app_label, model.__class__.__name__.lower())
+    return PREFIXMAP[key]
+
+
+def shorturl(obj):
+    try:
+        prefix = _get_prefix(obj)
+    except (AttributeError, KeyError):
+        return ''
+
+    tinyid = converter.from_decimal(obj.pk)
+    if hasattr(settings, 'SHORT_BASE_URL') and settings.SHORT_BASE_URL:
+        return urlparse.urljoin(settings.SHORT_BASE_URL, prefix + tinyid)
+
+    try:
+        return urlresolvers.reverse('shorturls.views.redirect', kwargs = {
+            'prefix': prefix,
+            'tiny': tinyid
+        })
+    except urlresolvers.NoReverseMatch:
+        return ''
+
+
 class ShortURL(template.Node):
     @classmethod
     def parse(cls, parser, token):
@@ -12,40 +40,19 @@ class ShortURL(template.Node):
         if len(parts) != 2:
             raise template.TemplateSyntaxError("%s takes exactly one argument" % parts[0])
         return cls(template.Variable(parts[1]))
-        
+
     def __init__(self, obj):
         self.obj = obj
-        
+
     def render(self, context):
         try:
             obj = self.obj.resolve(context)
         except template.VariableDoesNotExist:
             return ''
-            
-        try:
-            prefix = self.get_prefix(obj)
-        except (AttributeError, KeyError):
-            return ''
-        
-        tinyid = converter.from_decimal(obj.pk)
-                
-        if hasattr(settings, 'SHORT_BASE_URL') and settings.SHORT_BASE_URL:
-            return urlparse.urljoin(settings.SHORT_BASE_URL, prefix+tinyid)
-        
-        try:
-            return urlresolvers.reverse('shorturls.views.redirect', kwargs = {
-                'prefix': prefix,
-                'tiny': tinyid
-            })
-        except urlresolvers.NoReverseMatch:
-            return ''
-            
-    def get_prefix(self, model):
-        if not hasattr(self.__class__, '_prefixmap'):
-            self.__class__._prefixmap = dict((m,p) for p,m in settings.SHORTEN_MODELS.items())
-        key = '%s.%s' % (model._meta.app_label, model.__class__.__name__.lower())
-        return self.__class__._prefixmap[key]
-        
+
+        return shorturl(obj)
+
+
 class RevCanonical(ShortURL):
     def render(self, context):
         url = super(RevCanonical, self).render(context)
@@ -54,6 +61,8 @@ class RevCanonical(ShortURL):
         else:
             return ''
 
+
 register = template.Library()
 register.tag('shorturl', ShortURL.parse)
 register.tag('revcanonical', RevCanonical.parse)
+register.filter('shorturl', shorturl)
